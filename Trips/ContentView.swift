5e5435039f85197bcc7bae33769a74675b2a9dfa
Ofek Grego <ofek.grego@gmail.com>
2019-10-18 20:35:39 +0300

    ofek first commit

--- Trips/ContentView.swift
+++ Trips/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
  
     var body: some View {
         TabView(selection: $selection){
-            Text("Fucking commit")
+            Text("Fucking ofek's commit")
                 .font(.title)
                 .tabItem {
                     VStack {
