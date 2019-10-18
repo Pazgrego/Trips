470e93b5632091b20f74b6c44129a48abe2051c8
Paz Grego <paz.grego1@gmail.com>
2019-10-18 19:07:04 +0300

    sign in\sign up buttons view

---
+++ Trips/Login.swift
@@ -0,0 +1,30 @@
+//
+//  Login.swift
+//  Trips
+//
+//  Created by Paz Grego on 18/10/2019.
+//  Copyright © 2019 Paz Grego. All rights reserved.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        VStack {
+            Text("Sign In/Sing Up").padding()
+            Button(action: {}) {
+            Text("Sign in")
+            }.padding()
+            Button(action: {}) {
+            Text("Sign up")
+            }.padding()
+        }
+        
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
